40ea6ed03021bc5de19e3660326cef546fdce972
Davidsiltroy <davidsiltroy2008@hotmail.com>
2021-08-14 17:26:59 -0500

    Improving the code to plot the data from the database

--- tesis_matplot.py
+++ tesis_matplot.py
@@ -1,119 +1,138 @@
+import time
 import matplotlib.pyplot as plt
 import numpy as np
-
-
 from tesis_sqlite import DB_sensors_sql
 
+"""Program to plot the data from my thesis database
+The program is mainly in Spanish"""
+
+#Global variables
 db_sql = DB_sensors_sql()
-
 all_from_test = db_sql.get_data_from_column("sensors_tests","*")
-
 tests_names=[]
-tests_numbers=0
+tests_selected = 0
+y_all=[]
+x_all=[]
+tests_selected_names = []
 
-print("Escriba el número del test que desea graficar: ")
+#function to check if the string can be changed to a int
+def checkNumber(string_number):
+    try:
+        number= int(string_number)
+        return True
+    except ValueError:
+        return False
 
+#taking all the names from the tests in the database
 for test in all_from_test:
-    tests_names.append(test[1])
-    print(f'[{tests_numbers}] = {test[1]}')
-    tests_numbers+=1
+        tests_names.append(test[1])
 
-n_selected= int(input("Número: "))
-print("Escriba el dominio en X")
-
-
-doc_name=tests_names[n_selected]
-
-print(f'El nombre seleccionado es: {doc_name}')
-
-data_from_sql = db_sql.get_data_from_column(doc_name,"*")
-yi=[]
-xi=[]
-yv=[]
-yb=[]
-ym=[]
-
-max_jump=0
-pos_max_jump=0
-last_val=0
-
-
-max_s_val=0
-min_s_val=3000
-yv_s=994 #valor que debe dar el sensor a determinado peso (10kg, 20kg, 40kg)
-# es a 994 para sensores FSR serie 400 a 10kg y 464 para df9-40 a 20kg
-yv_r_s=660 #valor que debe dar el sensor a con peso distribuido
-# FSR400 => 393 , 660, 769
-# FSR402 => 786 , 917, 954
-# DF9-40 => 73 , 111.
-the_label= 'valor del sensor a 0,036kg'
-
-print(f'Se encontraron {len(data_from_sql)} datos')
-from_part=int(input("Desde X en: "))
-to_part=int(input("Hasta X en: "))
-a=0
-total_data=0
-for data in data_from_sql:
-    if from_part <= a:
-        xi.append(a)
-        yi.append(data[1])
-        yv.append(yv_s)
-        yb.append(yv_r_s)
-        total_data=total_data+data[1]
-        if a>0:
-            current_jump=abs(last_val-data[1])
-            if current_jump>max_jump:
-                max_jump=current_jump
-                pos_max_jump=a
-        if max_s_val<data[1]:
-            max_s_val=data[1]
-        if min_s_val>data[1]:
-            min_s_val=data[1]
-        if to_part<=a:
-            break
-    a+=1
-    last_val = data[1]
-
-med_val= total_data/a
+#just to left space in the terminal
 print("")
-print(f'El valor mínimo en la gráfica es de {min_s_val}')
-print(f'El valor máximo en la gráfica es de {max_s_val}')
-print(f'El valor medio en la gráfica es de {med_val}')
-print(f'El salto más grande es de {max_jump} en la pisición {pos_max_jump}')
+print("")
 
-volt_min = 5*min_s_val/1023
-volt_max = 5*max_s_val/1023
+#Taking the main data to show the values of the tests
+while True:
+    print("Escriba el número del test que desea graficar o sólo de enter para continuar: ")
+    time.sleep(1)
+    tests_numbers=0
+    for test in tests_names:
+        print(f'[{tests_numbers}] = {test}')
+        tests_numbers+=1
+        time.sleep(0.01)
+    ask_number=input("Número: ")
+    if not checkNumber(ask_number):
+        if tests_selected>1:
+            print("No hay número, continuemos")
+        else:
+            print("No se seleccionó ningún test")
+        break
+    n_selected= int(ask_number)
+    if n_selected>=tests_numbers:
+        print("Número mayor a la cantidad de tests")
+        time.sleep(1)
+        print("Intenta de nuevo..")
+        time.sleep(1)
+    elif n_selected < 0:
+        print("¿Qué pretendes?..")
+        time.sleep(2)
+        print("Vamos en serio esta vez..")
+        time.sleep(2)
+    else:
+        doc_name=tests_names[n_selected]
+        time.sleep(0.1)
+        print(f'Se ha seleccionado: {doc_name}')
+        tests_selected_names.append(doc_name)
+        time.sleep(0.2)
+        print("Escriba el dominio en X")
+        time.sleep(0.2)
+        #taking the data for the plot
+        data_from_sql = db_sql.get_data_from_column(doc_name,"*")
+        print(f'Se encontraron {len(data_from_sql)} datos')
+        time.sleep(0.2)
 
-print(f'El voltaje mínimo en la gráfica es de {volt_min}')
-print(f'El voltaje máximo en la gráfica es de {volt_max}')
+        #seting a default start and end of the plot
+        from_part=0
+        to_part = len(data_from_sql)
+        #taking the start of the plot
+        while True:
+            ask_from_part = input("Desde X en: ")
+            if checkNumber(ask_from_part):
+                from_part=int(ask_from_part)
+                if from_part<0:
+                    print("Ese número está por debajo de 0")
+                else:
+                    break
+            else:
+                print(f'Ingresa un número entre 0 a {len(data_from_sql)}')
+        #taking the end of the plot
+        while True:
+            ask_to_part =input("Hasta X en: ")      
+            if checkNumber(ask_to_part):
+                to_part=int(ask_to_part)
+                if to_part<1:
+                    print("Ese número está por debajo de 0")
+                elif to_part>len(data_from_sql):
+                    print(f'Sólo se mostrarán los datos hasta el punto {len(data_from_sql)}')
+                    break
+                else:
+                    break
+            else:
+                print(f'Ingresa un número entre 1 a {len(data_from_sql)}')
+        print("")
+        print("Si desea agregar un nuevo test..")
+        tests_selected+=1
+        a=0
+        x_i=[]
+        y_i=[]
+        for data in data_from_sql:
+            if from_part <= a:
+                x_i.append(a-from_part)
+                y_i.append(data[1])
+                if to_part<=a:
+                    break
+            a+=1
+        x_all.append(x_i)
+        y_all.append(y_i)
 
-for i in xi:
-    ym.append(med_val)
 
-
-x = np.array(xi)
-y = np.array(yi)
-y1 = np.array(yv)
-y2 = np.array(yb)
-y3 = np.array(ym)
-
-
-A = np.vstack([x, np.ones(len(x))]).T
-
-
-
-m, c = np.linalg.lstsq(A, y, rcond=None)[0]
-
-print(f'La función de la recta es y= {m}*x + {c}')
-
-_ = plt.ylabel(f'Valor de sensor')
-_ = plt.xlabel("Número de repeticiones")
-#_ = plt.plot(x, y, label='líneas de continuidad', markersize=1)
-_ = plt.plot(x, y, 'o', label='Valor de sensor', markersize=1)
-#_ = plt.plot(x, y1, label='valor del sensor a 10kg', markersize=3)
-_ = plt.plot(x, y3, label='Valor Promedio', markersize=8)
-#_ = plt.plot(x, y2, label=the_label, markersize=4)
-_ = plt.plot(x, m*x + c, 'r', label=f'Recta que mejor se ajusta', markersize=9)
-_ = plt.legend()
-plt.title(f'{doc_name}')
-plt.show()
+print("")
+print("Si desea agregar un título, escribalo o sino apaste Enter")
+the_title = input("Título: ")
+if tests_selected>0:
+    print("Graficando..")
+    a=0
+    _ = plt.ylabel(f'Valor de sensor')
+    _ = plt.xlabel("Número de repeticiones")
+    for test_name in tests_selected_names:
+        x = np.array(x_all[a]) 
+        y = np.array(y_all[a])
+        _ = plt.plot(x, y, 'o', label=f'Valor de sensor {test_name}', markersize=2)
+        a+=1
+    _ = plt.legend()
+    if len(the_title)>0:
+        plt.title(the_title)
+    plt.show()
+    print("Cerrando el programa")
+else:
+    print("Cerrando el programa")
